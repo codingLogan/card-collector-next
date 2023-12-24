7a2af144f27f3fd39702ace8203a8c79d8605d13
codingLogan <reachCodingLogan@gmail.com>
2023-12-24 12:02:02 -0700

    Add pages for collection, sets, and storage

---
+++ src/app/collection/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-16">
+            <h1 className="font-bold text-3xl">Collection</h1>
+        </main>
+    )
+}
